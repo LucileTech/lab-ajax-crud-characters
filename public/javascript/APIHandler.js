e21a86fc447a8b51e147ca5166785e7ef7afabcf
LucileTech <lucile.t.tech@gmail.com>
2022-11-10 21:41:43 +0100

    done but issue with page refreshing even with prevent default as in the morning class

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -1,25 +1,47 @@
 class APIHandler {
-  constructor (baseUrl) {
+  constructor(baseUrl) {
     this.BASE_URL = baseUrl;
   }
 
-  getFullList () {
+  //   router.get("/characters", (req, res, next) => {
+  //     //Get all the characters info from http://localhost:8000/characters
+  //     res.json({ "http://localhost:8000/characters" });
+  //   });
 
-  }
+  //   router.get("/characters/:id", (req, res, next) => {
+  //     const { id } = req.params
+  //     res.json({ `http://localhost:8000/characters/${id}` });
+  //   });
 
-  getOneRegister () {
+  //   router.post("/characters", (req, res, next) => {
+  //     try {
+  //       const { name: string, occupation: string, cartoon: boolean, weapon: string } = req.body
+  //       console.log(req.body);
+  //     } catch (error) {
+  //       next(error)
+  //     }
+  //   });
 
-  }
+  //   router.patch("/characters/:id", async (req, res, next) => {
+  //     try {
+  //       const { id } = req.params
+  //       const { name: string, occupation: string, cartoon: boolean, weapon: string } = req.body
+  //       const updatedCharactere = await req.body.findByIdAndUpdate(id, req.body, {
+  //         new: true,
+  //       })
+  //       res.json(updatedCharactere)
+  //     } catch (error) {
+  //       next(error)
+  //     }
+  //   })
 
-  createOneRegister () {
+  //   router.delete("/characters/:id", async (req, res, next) => {
+  //     try {
+  //       await req.body.findByIdAndDelete(req.params.id)
 
-  }
-
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
-  }
+  //       res.json({ message: "Character has been successfully deleted" })
+  //     } catch (error) {
+  //       next("Character not found")
+  //     }
+  //   })
 }
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,191 @@
-const charactersAPI = new APIHandler('http://localhost:8000');
+const charactersAPI = new APIHandler("http://localhost:8000");
+const editName = document.getElementsByClassName("name");
+const editOccupation = document.getElementsByClassName("occupation");
+const editCartoon = document.getElementsByClassName("cartoon");
+const editWeapon = document.getElementsByClassName("weapon");
+const characterInfo = document.querySelector(".character-info");
+const characterContainer = document.querySelector(".characters-container");
 
-window.addEventListener('load', () => {
-  document.getElementById('fetch-all').addEventListener('click', function (event) {
+// async function fetchCharacters() {
+// 	const { data } = await axios.get("http://localhost:3000/phone")
+// 	characterList.innerHTML = ""
+// 	data.forEach((character) => {
+// 		const li = document.createElement("li")
+// 		li.innerHTML = `<span class="name">${character.name}</span> cost
+//     $<span class="price">${character.price}</span>
+//     <span class="edit"><i data-id="${character._id}" class="fa-solid fa-pen-to-square"></i></span>
+//     <span class="delete"><i class="fa-solid fa-trash"></i></span>`
+// 		// We might need the id later on (To update or delete !)
+// 		// Storing it inside of a dataset.
+// 		li.dataset.id = character._id
+// 		characterList.append(li)
+// 	})
+// }
 
-  });
+/**
+ * Event listeners
+ */
 
-  document.getElementById('fetch-one').addEventListener('click', function (event) {
+// function fetchCharacter(character) {
+//   const { name, occupation, weapon, cartoon } = character;
+//   const template = document.querySelector(".character-card");
+//   const clone = template.content.cloneNode(true);
+//   let characterName = clone.querySelector(".name");
+//   let characterOccupation = clone.querySelector(".occupation");
+//   let characterWeapon = clone.querySelector(".weapon");
+//   let characterCartoon = clone.querySelector(".cartoon");
 
-  });
+//   characterName.innerHTML += `<span> ${name}</span>`;
+//   characterOccupation.innerHTML += `<span> ${occupation}</span>`;
+//   characterWeapon.innerHTML += `<span> ${weapon}</span>`;
+//   characterCartoon.innerHTML += `<span> ${cartoon}</span>`;
 
-  document.getElementById('delete-one').addEventListener('click', function (event) {
+//   characterContainer.appendChild(clone);
+// }
 
-  });
+window.addEventListener("load", () => {
+  //TEST OK FETCH ALL
+  document
+    .getElementById("fetch-all")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      axios.get(`http://localhost:8000/characters`).then((response) => {
+        const allCharacters = response.data;
+        allCharacters.forEach((character) => {
+          const clone = characterInfo.cloneNode(true);
+          clone.querySelector(".name").innerText = character.name;
+          clone.querySelector(".occupation").innerText = character.occupation;
+          clone.querySelector(".cartoon").innerText = character.cartoon;
+          clone.querySelector(".weapon").innerText = character.weapon;
+          characterContainer.append(clone);
+        });
+      });
+      // .catch((err) => console.log("Character not found "))
+    });
 
-  document.getElementById('edit-character-form').addEventListener('submit', function (event) {
+  // const { data } = await axios.get("http://localhost:8000/characters");
+  // console.log(data);
+  // characterList.innerHTML = "";
+  // data.forEach((character) => {
+  //   // console.log(editName.innerHTML);
+  //   // const li = document.createElement("li")
+  //   editName.innerHTML += `: ${character.name}`;
+  //   // editOccupation.innerHTML += `${character.occupation}`,
+  //   // editCartoon.innerHTML += `${character.cartoon}`,
+  //   // editWeapon.innerHTML += `${character.weapon}`,
+  // });
 
-  });
+  // data.forEach((character) => {
+  //   const li = document.createElement("li");
+  //   li.innerHTML = `<span class="name">${character.name}</span> cost
+  //   $<span class="price">${character.price}</span>
+  //   <span class="edit"><i data-id="${character._id}" class="fa-solid fa-pen-to-square"></i></span>
+  //   <span class="delete"><i class="fa-solid fa-trash"></i></span>`;
+  //   // We might need the id later on (To update or delete !)
+  //   // Storing it inside of a dataset.
+  //   li.dataset.id = character._id;
+  // characterList.append(li);
 
-  document.getElementById('new-character-form').addEventListener('submit', function (event) {
+  //TEST OK DELETE ONE
+  document
+    .getElementById("delete-one")
+    .addEventListener("click", async function (event) {
+      event.preventDefault();
+      console.log(document.getElementsByTagName("input")[1].value);
+      const { data } = await axios.delete(
+        `http://localhost:8000/characters/${
+          document.getElementsByTagName("input")[1].value
+        }`
+      );
+      console.log("Character has been successfully deleted");
+    });
 
-  });
+  document
+    .getElementById("fetch-one")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      axios
+        .get(
+          `http://localhost:8000/characters/${
+            document.getElementsByTagName("input")[0].value
+          }`
+        )
+        .then((response) => {
+          console.log(response.data);
+          const clone = characterInfo.cloneNode(true);
+          const oneCharacter = response.data;
+          document.querySelector(".name").innerText = oneCharacter.name;
+          document.querySelector(".occupation").innerText =
+            oneCharacter.occupation;
+          document.querySelector(".cartoon").innerText = oneCharacter.cartoon;
+          document.querySelector(".weapon").innerText = oneCharacter.weapon;
+          characterContainer.append();
+        });
+      // .catch((err) => console.log("Error while getting the data: ", err))
+    });
+
+  document
+    .getElementById("create-data")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      let character = {};
+      character.name = document.getElementsByTagName("input")[2].value;
+      character.occupation = document.getElementsByTagName("input")[3].value;
+      character.weapon = document.getElementsByTagName("input")[4].value;
+      character.cartoon = document.getElementsByTagName("input")[5].value;
+      console.log(character);
+      axios
+        .post(`http://localhost:8000/characters/`, character)
+        .then((response) => {
+          console.log(response.data);
+          const newCharacter = response.data;
+          document.querySelector(".name").innerText = newCharacter.name;
+          document.querySelector(".occupation").innerText =
+            newCharacter.occupation;
+          document.querySelector(".cartoon").innerText = newCharacter.cartoon;
+          document.querySelector(".weapon").innerText = newCharacter.weapon;
+          characterContainer.append(clone);
+        });
+    });
+
+  document
+    .getElementById("edit-character-form")
+    .addEventListener("submit", async function (event) {
+      event.preventDefault();
+      let character = {};
+      character.id = document.getElementById("the-id").value;
+      character.name = document.getElementsByTagName("input")[7].value;
+      character.occupation = document.getElementsByTagName("input")[8].value;
+      character.weapon = document.getElementsByTagName("input")[9].value;
+      character.cartoon = document.getElementsByTagName("input")[10].value;
+      const id = document.getElementById("the-id").value;
+      console.log(id);
+      axios
+        .patch(`http://localhost:8000/characters/${id}`, character)
+        .then((response) => {
+          console.log(response.data);
+          const newCharacter = response.data;
+          document.querySelector(".name").innerText = newCharacter.name;
+          document.querySelector(".occupation").innerText =
+            newCharacter.occupation;
+          document.querySelector(".cartoon").innerText = newCharacter.cartoon;
+          document.querySelector(".weapon").innerText = newCharacter.weapon;
+          characterContainer.append(clone);
+        });
+      // .catch((err) => console.log("Character not found "))
+      // const url = `http://localhost:8000/characters/${id}`;
+      // try {
+      //   const updatedCHaracter = await axios({
+      //     method: "PATCH",
+      //     baseURL: url,
+      //     data: character,
+      //   });
+      //   const { data } = await axios.patch(url, character);
+      //   await axios({
+      //     method: "PATCH",
+      //   });
+      // } catch (error) {
+      //   console.log(error);
+      // }
+    });
 });
